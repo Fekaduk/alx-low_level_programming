fc2884869a0528bb3090760f2046dc167c315248
Fekaduk <fekadu9985@gmail.com>
2023-11-22 09:21:31 -0800

    task0 update

--- 0x13-more_singly_linked_lists/0-main.c
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -5,6 +5,7 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -13,6 +14,7 @@ int main(void)
 	listint_t *new;
 	listint_t hello = {8, NULL};
 	size_t n;
+
 	head = &hello;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
